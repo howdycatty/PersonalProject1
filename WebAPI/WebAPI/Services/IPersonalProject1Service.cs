b8e8f1aa5ff8a9998be075d198dc40ff96fac090
howdycatty <howdycatty@yahoo.com>
2019-02-19 20:57:29 -0800

    initial commit

---
+++ WebAPI/WebAPI/Services/IPersonalProject1Service.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public interface IPersonalProject1Service
+    {
+        int Create(PersonalProject1Create request);
+        List<PersonalProject1> GetAll();
+    }
+}
